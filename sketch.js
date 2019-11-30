b4af0b0319bfd8e413404b6dd2d4297cac4f6a4c
Xohan Otero Barbosa <xohan.otero@gmail.com>
2019-11-30 04:14:52 +0100

    Finished Code
    TO-DO Add button to train & test

--- sketch.js
+++ sketch.js
@@ -31,58 +31,17 @@ function preload(){
 }
 
 
-/* function viewImages(){
-  let total = 100;
-  for (let n = 0; n < total; n++){
-    let img = createImage(28,28);
-    img.loadPixels();
-    let offset = n * len;
-    for(let i = 0; i < len; i++){
-      let val = 255 - apples.bytes[i + offset];
-      //Flere fordi farger
-      img.pixels[i*4] = val;
-      img.pixels[i*4+1] = val;
-      img.pixels[i*4+2] = val;
-      img.pixels[i*4+3] = 255;
 
-    }
-    img.updatePixels();
-    let x = (n%10) * 28;
-    let y = floor(n/10) * 28;
-    image(img,x,y);
-  }
-} */
 
-function setup() {
-createCanvas(280,280);
-background(255);
-//viewImages();
 
-prepareData(apples_object,apples, APPLE);
-prepareData(frogs_object,frogs, FROG);
-prepareData(clocks_object, clocks, CLOCK);
-prepareData(mona_object,mona, MONA);
-prepareData(bees_object,bees, BEE);
-NN = new NeuralNetwork(784, 64, 5);//784 inputs, 64 hidden nodes, and 5 outputs, 1 output for each category
-
+function trainEpoch(trainingArray){
 //Train to learn about category
-let trainingArray = [];
-trainingArray = trainingArray.concat(mona_object.training);
-trainingArray = trainingArray.concat(bees_object.training);
-trainingArray = trainingArray.concat(clocks_object.training);
-trainingArray = trainingArray.concat(frogs_object.training);
-trainingArray = trainingArray.concat(apples_object.training);
-
 shuffle(trainingArray,true);
 //console.log(trainingArray);
 //Train for one epoch
 for (let i = 0; i < trainingArray.length;i++){
-  let inputs = [];
   let data = trainingArray[i];
-  
-  for(let j = 0; j< data.length;j++){
-    inputs[j] = data[j] / 255.0; //Normalize Data
-  }
+  let inputs = Array.from(data).map(x => x/255);
   let label = trainingArray[i].label;
   let targets = [0,0,0,0,0];
   targets[label] = 1;
@@ -91,6 +50,104 @@ for (let i = 0; i < trainingArray.length;i++){
 }
 }
 
+
+function setup() {
+createCanvas(280,280);
+background(0);
+//viewImages();
+let guessButton = select('#guess');
+guessButton.mousePressed(function() {
+  let inputs = [];
+  let img = get();
+  img.resize(28,28);
+  img.loadPixels();
+  for(let i = 0; i < len; i++){
+    
+    let bright = img.pixels[i];
+    inputs[i] = bright / 255.0;
+  }
+  //console.log(inputs);
+
+  let guess = NN.predict(inputs);
+  let m = max(guess);
+  let classification = guess.indexOf(m);
+
+  if(classification === APPLE) {
+    console.log("apple");
+  }else if(classification === MONA) {
+    console.log("Mona");
+  }else if(classification === BEE) {
+    console.log("Bee");
+  }else if(classification === FROG) {
+    console.log("Frog");
+  }else if(classification === CLOCK) {
+    console.log("Clock");
+  }
+
+});
+let clearButton = select('#clear');
+clearButton.mousePressed(function(){
+  background(0);
+})
+//Prepares data for Neural Network, giving all values a label
+prepareData(apples_object,apples, APPLE);
+prepareData(frogs_object,frogs, FROG);
+prepareData(clocks_object, clocks, CLOCK);
+prepareData(mona_object,mona, MONA);
+prepareData(bees_object,bees, BEE);
+
+
+NN = new NeuralNetwork(784, 64, 5);//784 inputs, 64 hidden nodes, and 5 outputs, 1 output for each category
+let trainingArray = [];
+trainingArray = trainingArray.concat(mona_object.training);
+trainingArray = trainingArray.concat(bees_object.training);
+trainingArray = trainingArray.concat(clocks_object.training);
+trainingArray = trainingArray.concat(frogs_object.training);
+trainingArray = trainingArray.concat(apples_object.training);
+
+let testingArray = [];
+testingArray = testingArray.concat(bees_object.testing);
+testingArray = testingArray.concat(mona_object.testing);
+testingArray = testingArray.concat(clocks_object.testing);
+testingArray = testingArray.concat(frogs_object.testing);
+testingArray = testingArray.concat(apples_object.testing);
+//console.log(testingArray.length);
+
+//Trains NN
+for (let i = 0;i < 5; i++){
+trainEpoch(trainingArray);
+console.log("Epochs trained:" + (i+1));
+testAll(testingArray);
+}
+}
+
+//Testing data
+function testAll(testingArray){
+  let correct = 0;
+
+  for (let i = 0; i < testingArray.length;i++){
+  
+    let data = testingArray[i];
+    let inputs = Array.from(data).map(x => x/255);
+    let label = testingArray[i].label;
+    let guess = NN.predict(inputs);
+   // console.log(guess);
+    let m = max(guess);
+    let classification = guess.indexOf(m);
+    //console.log(classification);
+    //console.log(label);
+    if ( classification === label){
+      correct++;
+    }
+
+
+
+  }
+  let percetage = correct / testingArray.length;
+console.log("% Correct:"+percetage);
+}
+
+//Preparing data
 function prepareData(category, data, label){
   category.training = [];
   category.testing = [];
@@ -108,3 +165,11 @@ function prepareData(category, data, label){
     }
   }
 }
+function draw(){
+  strokeWeight(8);
+  stroke(255);
+  if(mouseIsPressed){
+  line(pmouseX,pmouseY,mouseX,mouseY);
+}
+}
+
